2ffa62d11294b34832a831d9a1e55a9ea4b043ff
Laco <vitmatula@gmail.com>
2022-02-16 00:32:50 +0100

    Flask tutorial all

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, url_for
+from second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix="/admin")
 
-@app.route("/home")
-@app.route("/")
+@app.route("/homee")
 def home():
 	return render_template("home.html")
+
+@app.route("/test")
+def test():
+	return "<h1>Test</h1>"
 
 if __name__=="__main__":
 	app.run(debug=True)
